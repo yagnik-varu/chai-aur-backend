3c61dcc0ca4251139b6829a9ccd48a5c581cee6d
yagnik-varu <yagnikvaru2003@gmail.com>
2024-04-23 11:33:22 +0530

    define user and video model and it's methods

---
+++ src/models/user.model.js
@@ -0,0 +1,84 @@
+import mongoose, { Schema, model } from 'mongoose'
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar: {
+        type: String,//cloud Inaryurl
+        required: true,
+    },
+    coverImage: {
+        type: String,//cloud Inaryurl
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    refreshToken: {
+        type: String
+    }
+
+}, { timestamps: true })
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        fullname: this.fullname,
+        username: this.username
+    }, process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        })
+}
+
+userSchema.methods.genrateRefreshToken = function () {
+    return jwt.sign({
+        _id: this._id,
+    }, process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+const User = model('User', userSchema);
+
+export default User
