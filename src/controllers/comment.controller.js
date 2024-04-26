862aefca7e2713524c68e79e00a8a9338965e539
yagnik-varu <yagnikvaru2003@gmail.com>
2024-04-26 10:44:35 +0530

    add pagination in comment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -7,52 +7,67 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     //PAGINATE IS IN PENDING
-    const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
-    
+    const { videoId } = req.params
+    const { page = 1, limit = 1 } = req.query
+    console.log(req.query)
+    var commentAggregate = Comment.aggregate();
+    Comment.aggregatePaginate(commentAggregate, { page, limit })
+        .then(function (result) {
+            console.log(result)
+            return res.status(200).json(
+                new ApiResponse(200,result,"comment paginate successfully")
+            )
+        })
+        .catch(function (err) {
+            console.log(err)
+            return res.status(400).json(
+                new ApiError(400,"something error in comment fetch",err.message)
+            )
+        })
+
 
 })
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
-    const {content} = req.body
+    const { content } = req.body
     // console.log(content)
     const { videoId } = req.params
-    if(!content){
-        throw new ApiError('401',"content is not present")
+    if (!content) {
+        throw new ApiError('401', "content is not present")
     }
-    const comment =await Comment.create(
+    const comment = await Comment.create(
         {
             content,
-            video:videoId,
-            owner:req.user._id
+            video: videoId,
+            owner: req.user._id
         }
     )
     return res.status(200).json(
-        new ApiResponse(200,comment,"comment added successfully")
+        new ApiResponse(200, comment, "comment added successfully")
     )
 })
 
 const updateComment = asyncHandler(async (req, res) => {
-    const {content} = req.body
+    const { content } = req.body
     const { commentId } = req.params
-    if(!content){
-        throw new ApiError('401',"content is not present")
+    if (!content) {
+        throw new ApiError('401', "content is not present")
     }
-    const updatedComment =await Comment.findByIdAndUpdate(
+    const updatedComment = await Comment.findByIdAndUpdate(
         commentId,
         {
             content
         },
         {
-            new:true
+            new: true
         }
     )
-    if(!updatedComment){
-        throw new ApiError(401,"your requested commnet id is invalid")
+    if (!updatedComment) {
+        throw new ApiError(401, "your requested commnet id is invalid")
     }
     return res.status(200).json(
-        new ApiResponse(200,updatedComment,"comment updated successfully")
+        new ApiResponse(200, updatedComment, "comment updated successfully")
     )
 })
 
@@ -60,21 +75,21 @@ const deleteComment = asyncHandler(async (req, res) => {
     try {
         const { commentId } = req.params
         await Comment.findOneAndDelete({
-            _id:commentId
+            _id: commentId
         })
         return res.status(200).json(
-            new ApiResponse(200,{},"comment deleted successfully")
+            new ApiResponse(200, {}, "comment deleted successfully")
         )
     } catch (error) {
-        throw new ApiError(401,"there is error in comment delete",error.message)
+        throw new ApiError(401, "there is error in comment delete", error.message)
     }
-    
-   
+
+
 })
 
 export {
-    getVideoComments, 
-    addComment, 
+    getVideoComments,
+    addComment,
     updateComment,
-     deleteComment
-    }
+    deleteComment
+}
