4e5d65ececddbe56f23e87c51adfbd73e3d27ab9
yagnik-varu <yagnikvaru2003@gmail.com>
2024-05-03 16:19:08 +0530

    fix issues of store video

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -15,8 +15,16 @@ const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description } = req.body
     console.log("req.body", req.body)
     console.log("req.files", req.files)
-    const videoLocalPath = req.files?.video[0]?.path
-    const thumbnailLocalPath = req.files?.thumbnail[0]?.path
+    let videoLocalPath;
+    if (req.files && Array.isArray(req.files.video) && req.files.video.length > 0) {
+        console.log("inside if")
+        videoLocalPath = req.files?.video[0]?.path;
+    }
+    let thumbnailLocalPath;
+    if (req.files && Array.isArray(req.files.thumbnail) && req.files.thumbnail.length > 0) {
+        console.log("inside if")
+        thumbnailLocalPath = req.files?.thumbnail[0]?.path;
+    }
 
     if ([title, description, videoLocalPath, thumbnailLocalPath].some((field) => field?.trim() === "")) {
         throw new ApiError(400, "All fields are required !!")
@@ -63,7 +71,7 @@ const publishAVideo = asyncHandler(async (req, res) => {
 
 const getAllVideos = asyncHandler(async (req, res) => {
     //TODO: get all videos based on query, sort, pagination
-    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
+    const { page = 1, limit = 5, query, sortBy, sortType, userId } = req.query
     let pipeline = [];
     console.group(userId)
     if (userId) {
@@ -85,11 +93,38 @@ const getAllVideos = asyncHandler(async (req, res) => {
         condition[sortBy] = (sortType === "desc") ? (Number(-1)) : (Number(1))
         pipeline.push({ $sort: condition })
     }
+
+    pipeline.push([
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "channelInfo",
+                pipeline: [
+                    {
+                        $project: {
+                            avatar: 1,
+                            email: 1,
+                            fullname: 1,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $addFields: {
+                channelInfo: {
+                    $first: "$channelInfo",
+                },
+            },
+        },
+    ])
     // ****** SORT TYPE IS NOT WORK WELL FIX IT AFTER
 
-    const videoAggregate =Video.aggregate(pipeline)
+    const videoAggregate = Video.aggregate(pipeline)
     //DON'T USE AWAIT HERE WE WANT QUERY I PAGINATE FUNCTION AWAIT GIVE ARRAY
-   
+
 
 
     Video.aggregatePaginate(videoAggregate, { page, limit })
@@ -106,7 +141,115 @@ const getAllVideos = asyncHandler(async (req, res) => {
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
-    const video = await Video.findById(req.params.id);
+    console.log("get video by id");
+    
+    console.log(req.user)
+    const currentUser = req.user._id;
+    const videoId = req.params.id;
+    const videoPipeline = [
+        {
+            $match:{
+                _id:mongoose.Types.ObjectId.createFromHexString(videoId)
+            }
+        },
+        {
+          $lookup: {
+            from: "comments",
+            localField: "_id",
+            foreignField: "video",
+            as: "commentsList",
+          },
+        },
+        {
+          $lookup: {
+            from: "subscriptions",
+            localField: "_id",
+            foreignField: "channel",
+            as: "subscriberList",
+            pipeline: [
+              {
+                $project: {
+                  _id: 0,
+                  subscriber: 1,
+                },
+              },
+            ],
+          },
+        },
+        {
+          $lookup: {
+            from: "likes",
+            localField: "_id",
+            foreignField: "video",
+            as: "likeList",
+          },
+        },
+        {
+          $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "channelInfo",
+              pipeline: [
+                  {
+                      $project: {
+                          avatar: 1,
+                          email: 1,
+                          fullname: 1,
+                      },
+                  },
+              ],
+          },
+      },
+      
+        {
+          $addFields: {
+            isSubscribed: {
+              $anyElementTrue: {
+                $map: {
+                  input: "$subscriberList",
+                  as: "item",
+                  in: {
+                    $eq: [
+                      "$$item.subscriber",
+                      currentUser,
+                    ],
+                  }, 
+                },
+              },
+            },
+            isLiked: {
+              $anyElementTrue: {
+                $map: {
+                  input: "$likeList",
+                  as: "item",
+                  in: {
+                    $eq: [
+                      "$$item.likedBy",
+                      currentUser,
+                    ],
+                  },
+                },
+              },
+            },
+            noOfLikes:{
+              $size:'$likeList'
+            },
+            noOfSubscriber:{
+              $size:'$subscriberList'
+            },
+            noOfComment:{
+              $size:'$commentsList'
+            },channelInfo: {
+              $first: "$channelInfo",
+          },
+          },
+        },
+      ]
+    const video = await Video.aggregate(videoPipeline);
+
+
+
     if (!video) {
         return res.status(400).json(
             new ApiResponse(400, {}, "invalid id")
--- src/router/video.route.js
+++ src/router/video.route.js
@@ -24,7 +24,7 @@ router.route('/')
 
 
 router.route('/:id')
-.get(getVideoById)
+.get(verifyJwt,getVideoById)
 .delete(deleteVideo)
 .patch(upload.single("thumbnail"), updateVideo);
 
